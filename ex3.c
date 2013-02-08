aebc0d4d5c941f56c8650a7648afa07602df8b2c
John Harrison <john.harrison@liv.ac.uk>
2013-02-08 10:13:43 +0000

    ex3: added string and character formatting

--- ex3.c
+++ ex3.c
@@ -2,12 +2,15 @@
 
 int main()
 {
-
+	char given_name[] = "John";
+	char family_name[] = "Harrison";
+        char initial = given_name[0];
 	int age = 33;
 	int height_in_cm = 182;
         float cm_in_foot = 183.0 / 6.0;
 	float height_in_feet = height_in_cm / cm_in_foot;
 
+	printf("My name is %c. %s.\n", initial, family_name);
 	printf("I am %d years old.\n", age);
 	printf("I am %dcm tall.\n", height_in_cm);
 	printf("I am %f feet tall.\n", height_in_feet);
