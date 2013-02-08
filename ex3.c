7ef01cee3a543faa421a33f117c61d267f80ba61
John Harrison <john.harrison@liv.ac.uk>
2013-02-08 10:56:59 +0000

    ex3: break it! don't init age value

--- ex3.c
+++ ex3.c
@@ -5,13 +5,13 @@ int main()
 	char given_name[] = "John";
 	char family_name[] = "Harrison";
         char initial = given_name[0];
-	int age = 33;
+	int age;
 	int height_in_cm = 182;
         float cm_in_foot = 183.0 / 6.0;
 	float height_in_feet = height_in_cm / cm_in_foot;
 
         printf("Name:\v%c. %s.\n", initial, family_name);
-	printf("Age:\v%d years.\n");
+	printf("Age:\v%d years.\n", age);
 	printf("Physical Description:\n");
 	printf("\tHeight in cm: %d\r", height_in_cm);
 	printf("\tHeight in feet: %.2f\n", height_in_feet);
