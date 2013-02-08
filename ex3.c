d52d658df3801fcda64178f56fb12cb676d001b9
John Harrison <john.harrison@liv.ac.uk>
2013-02-08 10:12:48 +0000

    ex3: added calculation and float formatting

--- ex3.c
+++ ex3.c
@@ -3,11 +3,14 @@
 int main()
 {
 
-	int age = 10;
-        int height = 72;
+	int age = 33;
+	int height_in_cm = 182;
+        float cm_in_foot = 183.0 / 6.0;
+	float height_in_feet = height_in_cm / cm_in_foot;
 
 	printf("I am %d years old.\n", age);
-	printf("I am %d inches tall.\n", height);
+	printf("I am %dcm tall.\n", height_in_cm);
+	printf("I am %f feet tall.\n", height_in_feet);
 
 	return 0;
 }
